cdb771e983584dda1e36a08f183ef346c9b6100f
FriedrichWeinmann <FriedrichWeinmann@users.noreply.github.com>
2025-08-31 10:39:02 +0200

    updated icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'PSFramework Project',
   tagline: 'The project dedicated to empowering your PowerShell scripting.',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/psframework.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
